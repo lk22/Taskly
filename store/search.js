d1ac9fa9bdef3136bcbc2ca26527b6e9dc12ca0e
Leo Knudsen <nightskater-220@hotmail.com>
2017-11-21 01:15:34 +0100

    creating search state to store

---
+++ store/search.js
@@ -0,0 +1,87 @@
+/**
+ * imports
+ */
+
+         import Vuex from 'vuex'
+
+         import {
+             makeRequest
+         } from '~/globals'
+
+         import {
+             api
+         } from '~/globals/API/endpoints'
+
+
+/**
+ * action types definitions
+ */
+
+        const types = {
+            SET_SEARCH_STRING: 'SET_SEARCH_STRING'
+        }
+
+
+/**
+ * define user state
+ */
+
+        const search = {
+            /**
+             * stored state
+             * @type {Object}
+             */
+            state: {
+                searchString: '',
+                searchResults: {}
+            },
+
+            /**
+             * actions
+             */
+            actions: {
+
+                /**
+                 * fetching search results through api
+                 * @param  {[type]} context [description]
+                 * @param  {[type]} string  [description]
+                 * @return {[type]}         [description]
+                 */
+                createSearchFromString(context, { string } ) {
+
+                    // making request to API
+                    makeRequest('GET', api + 'search?string=' + string).then((results) => {
+
+                        // storing the results
+                        const searchData = results.data
+                        console.log(searchData)
+
+                        // committing action
+                        context.commit(types.SET_SEARCH_STRING, {
+                            ...searchData
+                        })
+
+                        // extending promise
+                        return Promise.resolve()
+                    }).catch((error) => {
+                        throw new Error(error)
+                    })
+                },
+
+                /**
+                 * action mutators
+                 * @type {Object}
+                 */
+                mutations: {
+                    [types.SET_SEARCH_STRING](state, results) {
+                        state.searchResults = results
+                    }
+                },
+
+                // allow store namespaces
+                namespaced: true
+            }
+
+        }
+
+export default search
