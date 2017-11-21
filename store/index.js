3cfadb718da34c02d49175aac6f747d985df0ca1
Leo Knudsen <nightskater-220@hotmail.com>
2017-11-21 01:16:34 +0100

    allow search store to be in persisted state

--- store/index.js
+++ store/index.js
@@ -1,21 +1,23 @@
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import user from './user'
+import search from './search'
 
 const plugins = []
 
 // if(process.env.VUE_ENV === 'client') {
-	
+
 // }
 
 const createStore = () => new Vuex.Store({
 	modules: {
-		user
+		user,
+		search
 	},
 	plugins: [
 		createPersistedState({
 			key: 'taskly',
-			paths: ['user']
+			paths: ['user', 'search']
 		})
 	],
 });
