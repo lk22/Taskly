58a5546dbc046cd079674a3779f44c29e10b45a3
Leo Knudsen <nightskater-220@hotmail.com>
2017-11-27 12:43:58 +0100

    add crud actions (task.js)

--- store/task.js
+++ store/task.js
@@ -54,4 +54,95 @@
                     return state.items.filter(task => task.id === id)
                 }
             },
+
+            actions: {
+
+                /**
+                 * creating a new task
+                 */
+                create(context, {name, priority}) {
+
+                    // make API request
+                    try {
+
+                        makeRequest('POST', api + 'tasks/create-task', {
+                            name,
+                            priority
+                        }).then( (response) => {
+                            console.log(response);
+
+                            const name = name;
+                            const priority = priority;
+
+                            context.commit(types.ADD_NEW_TASK, {
+                                name,
+                                priority
+                            })
+
+                            return Promise.resolve();
+                        })
+
+                    } catch ( error ) {
+                        // throw error here
+                    }
+                },
+
+
+                /**
+                 * updating existing task
+                 */
+                update(context, {name, priority, id}) {
+
+                    // make API request
+                    try {
+
+                        makeRequest('POST', api + 'tasks/' + id + '/update-task', {
+                            name,
+                            priority
+                        }).then((response) => {
+                            console.log(response);
+
+                            const name = name;
+                            const priority = priority;
+
+                            context.commit(types.UPDATE_TASK, {
+                                name,
+                                priority
+                            })
+
+                            return Promise.resolve();
+                        })
+                    } catch (error) {
+
+                    }
+                },
+
+                /**
+                 * remove existing task
+                 */
+                delete(context, {id}) {
+
+                    const id = id;
+
+                    try {
+
+                        makeRequest('POST', api + 'tasks/' + id + '/delete-task', {}).then((response) => {
+                            console.log(response);
+
+                            context.commit(types.REMOVE_TASK);
+
+                            return Promise.resolve();
+                        })
+
+                    } catch (error) {}
+
+                },
+
+                /**
+                 * @todo
+                 * 1: add setPriority action Here
+                 * 2: checkoutTask action
+                 * 3: checkAllTasks action
+                 */
+            }
         }
