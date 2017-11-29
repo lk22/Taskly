fcb5c18731a74a648081d97c76c9ffa3eea8d3d6
Leo Knudsen <nightskater-220@hotmail.com>
2017-11-29 23:17:30 +0100

    collecting all tasklists to tasklists dashboard page

--- pages/app/dashboard/tasklists/index.vue
+++ pages/app/dashboard/tasklists/index.vue
@@ -49,19 +49,53 @@
             <div class="left">
                 <h3>Tasklists</h3>
             </div>
+
+            <div class="right">
+                <!-- settings button goes here -->
+            </div>
         </div>
+
 
         <!-- wrap this list in a seperate component -->
         <div class="tasklists-container__list">
           <div class="tasklist" v-if="tasklists" v-for="tasklist in tasklists">
-              <p class="name">{{ tasklist.name }}</p>
-              <p>{{ tasklist.id }}</p>
+              <table class="tasklist-table">
+                  <thead class="tasklist-table__header">
+                      <tr class="tasklist-table__heading">
+                          <td class="heading-name">name</td>
+                          <td class="heading-tasks__count">tasks count</td>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>{{ tasklist.name }}</td>
+                          <td>{{ tasklist.tasks_count }}</td>
+                      </tr>
+                  </tbody>
+              </table>
           </div>
         </div>
-
-
     </div>
 </template>
 
 <style lang="scss">
+
+    .tasklists-container{
+        padding:2rem;
+
+        .tasklists-container__header{
+            font-size: 30px;
+            min-height: 70px;
+            border-bottom: 1px solid #809ed3;
+        }
+
+        .tasklists-container__list{
+            padding:1.5rem;
+
+            .tasklist{
+
+            }
+        }
+    }
+
 </style>
